70dd6f59f613638dd1e17f48ba0926ef45886d33
Felipe <felipesouz4dev@gmail.com>
2021-07-12 23:39:05 -0300

    starting point

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'spectre.css/dist/spectre-icons.min.css'
+import 'spectre.css/dist/spectre.min.css'
+
 
 createApp(App).mount('#app')
